10969a5d1e5c07d61c6d941be19f8f651b3a2660
17saumyatripathi <17saumyatripathi@gmail.com>
2024-11-18 12:59:22 +0530

    netflix-clone

---
+++ day12/netflix-clone/src/App.jsx
@@ -0,0 +1,16 @@
+import MovieList from "./MovieList";
+
+const App = () => {
+  return (
+    <div><h1 style={{backgroundColor:"black",
+      color:"red",
+      fontSize:"50px",
+       textAlign:"center"}}>
+        Netflix Clone App
+        </h1>
+        <MovieList/>
+        </div>
+  );
+};
+
+export default App;
---
+++ day12/netflix-clone/src/movie.js
@@ -0,0 +1,38 @@
+const movies=[
+    {
+        id: 1,
+        image:"https://rukminim2.flixcart.com/image/612/612/xif0q/movie/4/f/r/2009-dvd-eagle-hindi-tarzan-the-wonder-car-original-imahfdpjpqg85j5d.jpeg?q=70",
+        title:"TARZAN THE WONDER CAR",
+        language:"Hindi",
+        cost:200,
+    },
+    {
+        id: 2,
+        image:"https://rukminim2.flixcart.com/image/612/612/xif0q/movie/d/5/q/2013-blu-ray-eagle-english-gupt-original-imagtxsb28ygkrhy.jpeg?q=70",
+        title:"GUPT",
+        language:"Hindi",
+        cost:420,
+    },
+    {
+        id: 3,
+        image:"https://rukminim2.flixcart.com/image/612/612/k3dc7m80/movie/t/c/f/2018-blu-ray-universal-pictures-excel-home-videos-english-original-imafmggqgub6vreg.jpeg?q=70",
+        title:"Jurassic World: Fallen Kingdom",
+        language:"English",
+        cost:620,
+    },
+    {
+        id: 4,
+        image:"https://rukminim2.flixcart.com/image/612/612/xif0q/movie/9/d/d/2013-blu-ray-eagle-hindi-golmaal-original-imagms36uz2hucpy.jpeg?q=70",
+        title:"GOLMAAL",
+        language:"Hindi",
+        cost:320,
+    },
+    {
+        id: 5,
+        image:"https://rukminim2.flixcart.com/image/612/612/xif0q/movie/4/s/t/2023-blu-ray-paramount-pictures-english-mission-impossible-dead-original-imaguw429xnwybtg.jpeg?q=70",
+        title:"Mission: Impossible Dead Reckoning Part",
+        language:"English",
+        cost:500,
+    },
+];
+export default movies;
